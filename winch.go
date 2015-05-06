85e0eab1999e69dbb9cb1c4ec85d21a92be15cd8
Erik Hollensbe <github@hollensbe.org>
2015-05-06 10:25:05 +0000

    Reorganization in progress

--- winch.go
+++ winch.go
@@ -1,18 +1,62 @@
-package termproxy
+package main
 
 import (
 	"net"
+	"os"
+	"sync"
 
-	term "github.com/erikh/termproxy/dockerterm"
+	"github.com/erikh/termproxy"
+	"github.com/erikh/termproxy/server"
 )
 
-type Winch struct {
-	Conn   net.Conn
-	Width  uint
-	Height uint
-}
+var (
+	connectionWinsizeMap = map[string]termproxy.Winch{}
+	winsizeMutex         = new(sync.Mutex)
+)
 
-func (w Winch) ToWinsize() *term.Winsize {
-	// the precision loss shouldn't matter
-	return &term.Winsize{Height: uint16(w.Height), Width: uint16(w.Width)}
+func compareAndSetWinsize(host string, ws termproxy.Winch, command *termproxy.Command, s *server.SSHServer) {
+	winsizeMutex.Lock()
+	connectionWinsizeMap[host] = ws
+
+	var height, width uint
+
+	for _, wm := range connectionWinsizeMap {
+		if height == 0 || width == 0 {
+			height = wm.Height
+			width = wm.Width
+		}
+		if wm.Height <= height {
+			height = wm.Height
+		}
+
+		if wm.Width <= width {
+			width = wm.Width
+		}
+	}
+
+	myws, _ := termproxy.GetWinsize(command.PTY().Fd())
+
+	if ws.Height != myws.Height || ws.Width != myws.Width {
+		// send the clear only in the height case, it will resolve itself with width.
+		termproxy.WriteClear(os.Stdout)
+		termproxy.SetWinsize(command.PTY().Fd(), ws)
+
+		s.Iterate(func(s *server.SSHServer, c net.Conn, index int) error {
+			payload := []byte{
+				0, 0, byte(ws.Width >> 8 & 0xFF), byte(ws.Width & 0xFF),
+				0, 0, byte(ws.Height >> 8 & 0xFF), byte(ws.Height & 0xFF),
+				0, 0, 0, 0,
+				0, 0, 0, 0,
+			}
+
+			c.(*server.Conn).SendRequest("window-change", false, payload)
+			if ws.Height != myws.Height {
+				termproxy.WriteClear(c)
+			}
+
+			return nil
+		})
+	}
+
+	winsizeMutex.Unlock()
 }
