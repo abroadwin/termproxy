688cecdd59139e1a69bcb65b0ed3d8a173cbde7b
Erik Hollensbe <github@hollensbe.org>
2015-05-08 08:19:11 +0000

    Update dist.sh to not build tp anymore

--- dist.sh
+++ dist.sh
@@ -3,11 +3,11 @@
 VERSION=$(cat VERSION)
 mypwd=${PWD}
 
-for arch in linux darwin freebsd windows
+for arch in linux darwin freebsd
 do
 	mkdir -p /tmp/termproxy-${arch}-${VERSION} && \
 	cd /tmp/termproxy-${arch}-${VERSION} && \
-	GOOS=$i go build github.com/erikh/termproxy/tp && \
+	GOOS=$i go build github.com/erikh/termproxy && \
 	cd .. && \
 	tar cvzf termproxy-${arch}-${VERSION}.tar.gz termproxy-${arch}-${VERSION} && \
   cp termproxy-${arch}-${VERSION}.tar.gz ${mypwd}	
