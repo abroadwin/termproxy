8bee248114b158ceb5a294ad5c2b8ce6c6448ccd
Erik Hollensbe <github@hollensbe.org>
2015-11-06 03:49:30 -0800

    main.go: use the end-user's id_rsa if it exists, this should ease users into termproxy use.

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 	usernameFlag = tp.StringOpt("u username", "scott", "Username for SSH")
 	passwordFlag = tp.StringOpt("p password", "tiger", "Password for SSH (set to '' to disable)")
-	hostkeyFlag = tp.StringOpt("k host-key", "host_key_rsa", "SSH private host key to present to clients")
+	hostkeyFlag = tp.StringOpt("k host-key", os.ExpandEnv("${HOME}/.ssh/id_rsa"), "SSH private host key to present to clients")
 	authorizedKeysFlag = tp.StringOpt("a authorized-keys", "", "SSH authorized hosts for public key authentication")
 	readOnly = tp.BoolOpt("r read-only", false, "Disallow remote clients from entering input")
 	notifications = tp.BoolOpt("n notifications", true, "Print notifications on connection and disconnection")
